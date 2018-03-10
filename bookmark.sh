6d21b618175d5ec372fbe7f26210ae546350b8fd
kristielim <kristie.lim@gmail.com>
2018-03-10 15:58:18 -0800

    checks for invalid arguments

--- bookmark.sh
+++ bookmark.sh
@@ -1,1 +1,32 @@
-echo " echo hello world " >> open.sh
+#! /bin/bash
+
+# this script stores currently running applications or open files into bookmark.log
+# each line of bookmark.log: <bookmark name> <file to open>
+
+if [ -z "$1" ]
+  then
+    echo "Pass in the name of the bookmark"
+    exit 1
+fi
+
+# check if Sublime is open
+if [ `ps -ax | grep -c Sublime` -gt 1 ]
+	then
+	echo "$1" -a \"Sublime Text\" >> bookmark.log
+fi
+
+# check if Xcode is open
+if [ `ps -ax | grep -c Xcode-beta` -gt 1 ]
+	then
+	echo "$1" -a Xcode-beta >> bookmark.log
+fi
+
+# check if Preview is open
+if [ `ps -ax | grep -c Preview` -gt 1 ]
+	then
+	ps -ax | grep Preview | cut -d ' ' -f 1 >> bookmark.log
+fi
+
+
+
+
--- open.sh
+++ open.sh
@@ -1,6 +1,10 @@
 #! /bin/bash
+
+# this script opens the bookmarks stored in bookmark.log
+# each line of bookmark.log: <bookmark name> <file to open>
+
 # if there is no argument, print out possible arguments
-if [ -z "$1" ]
+if [ -z "$1" ] || [ $1 = '-h' ]
   then
     echo "Possible arguments:"
     # print out all possible arguments
@@ -8,6 +12,12 @@ if [ -z "$1" ]
     exit 1
 fi
 
-# replace all instances of the bookmark name with open
-# then run in bash
-grep "^$1 " bookmark.log | sed "s/$1/open/" | bash
+# check if argument is found in bookmark.log
+if [ `grep -c "^$1 " bookmark.log` -gt 0 ]
+	then
+	# replace all instances of the bookmark name with open
+	# then run in bash
+	grep "^$1 " bookmark.log | sed "s/$1/open/" | bash
+else
+	echo "Bookmark not found"
+fi
